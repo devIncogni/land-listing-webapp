99f5e19fcccdf037bbd26bd028ec40b6be9bda9f
devIncogni <arinkumar10@outlook.com>
2025-07-04 03:24:06 +0530

    Added animation to hamburger menu icon

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,11 +1,28 @@
 function toggleExtendedNavMenu() {
-  const extendedMenu = document.querySelector(".extended-menu");
+  document.querySelector(".extended-menu").classList.toggle("expanded");
+}
 
-  extendedMenu.classList.toggle("expanded");
+function toggleHamburgerCross() {
+  document.querySelector(".hamburger").classList.toggle("cross");
+}
+
+function toggleHeaderTransparency() {
+  if (window.scrollY < 100) {
+    document
+      .querySelector(".header-extended-menu-container")
+      .classList.add("transparent-container");
+  } else {
+    document
+      .querySelector(".header-extended-menu-container")
+      .classList.remove("transparent-container");
+  }
 }
 
 const headerHamburger = document.querySelector(".hamburger-menu");
 
-headerHamburger.addEventListener("click", toggleExtendedNavMenu);
-
+headerHamburger.addEventListener("click", () => {
+  toggleExtendedNavMenu();
+  toggleHamburgerCross();
+});
+document.addEventListener(`scroll`, toggleHeaderTransparency);
 // toggleExtendedNavMenu();
