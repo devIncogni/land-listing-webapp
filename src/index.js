6904f37afd111505b56b08cac1e6bc32d8e5f830
devIncogni <arinkumar10@outlook.com>
2025-07-09 11:48:00 +0530

    Added a Explore properties button without functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles/default-reset.css";
 import "./styles/styles.css";
 import "./styles/header-styles.css";
 import "./styles/slideshow-styles.css";
+import "./styles/text-underline-animation.css";
 import "./styles/util-styles.css";
 // JS Imports
 import "./scripts/header";
