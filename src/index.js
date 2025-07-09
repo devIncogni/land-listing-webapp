5a73e1a16fe15009632e598c9a7543090f63ae2c
devIncogni <arinkumar10@outlook.com>
2025-07-09 18:20:51 +0530

    Added one single card and laid out it's designs

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import "./styles/styles.css";
 import "./styles/header-styles.css";
 import "./styles/slideshow-styles.css";
 import "./styles/text-underline-animation.css";
+import "./styles/land-card-styles.css";
 import "./styles/util-styles.css";
 // JS Imports
 import "./scripts/header";
 import "./scripts/loader";
 import "./scripts/slideshow";
-
-// Rellax(".slide-show-img.active");
-// const rel1 = new Rellax('.heading')
