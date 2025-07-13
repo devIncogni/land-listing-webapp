457d516344fb27123d008042bc39da56f32f1fd4
devIncogni <arinkumar10@outlook.com>
2025-07-13 12:01:16 +0530

    Wrote a JS function to populate images

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,7 +1,7 @@
 async function getData() {
   let data = await fetch("property-cards.json");
   data = await data.json();
-  console.log(data[1]);
+  return data;
 }
 
 getData();
@@ -10,12 +10,69 @@ function populateWithSkeletons() {
   const cardTemplate = document.querySelector(".card-template");
   const cardContainer = document.querySelector(".cards-container");
 
-  cardContainer.textContent = '';
+  cardContainer.textContent = "";
 
   for (let i = 0; i < 6; i += 1) {
     const cloneCard = cardTemplate.cloneNode(true);
+    cloneCard.classList.remove("card-template");
+    cloneCard.classList.add(`card-${i + 1}`);
     cardContainer.append(cloneCard);
   }
 }
 
-populateWithSkeletons()
+populateWithSkeletons();
+
+async function fillData(card, dataObj) {
+
+  const img = card.querySelector(".card-img-holder>img");
+  const plotSize = card.querySelector("[data-plot-size]");
+  const cardTitle = card.querySelector(".card-title");
+  const cardLocation = card.querySelector(".card-location > p");
+  const price = card.querySelector("[data-price]");
+  const plurality = card.querySelector("[data-plot-plurality]");
+  const btn = card.querySelector(".card-button-container>button>p");
+  const tagsDiv = card.querySelector(".tags");
+  const locationImg = card.querySelector(".card-location> img");
+
+  img.setAttribute("src", dataObj.image);
+  locationImg.setAttribute("src", "location.svg");
+  plotSize.textContent = dataObj.plotSize;
+  cardTitle.textContent = dataObj.cardTitle;
+  cardLocation.textContent = dataObj.cardLocation;
+  price.textContent = dataObj.price;
+  plurality.textContent = dataObj.plurality;
+  btn.textContent = "View Details";
+
+  for (let i = 0; i < 3; i += 1) {
+    const tag = document.createElement("p");
+    tag.textContent = dataObj.tags[i];
+    if (i === 2) {
+      tag.textContent = `+${dataObj.tags.length - i} More`;
+    }
+    tagsDiv.append(tag);
+  }
+}
+
+function removeSkeletons(card) {
+  card.querySelectorAll(".skeleton-text").forEach((s) => {
+    s.remove();
+  });
+
+  card.querySelectorAll(".skeleton").forEach((s) => {
+    s.classList.remove("skeleton");
+  });
+}
+
+async function populateSkeletons() {
+  const cards = document.querySelectorAll(".card");
+  const data = await getData();
+
+  for (let i = 0; i < 6; i += 1) {
+    const currentCard = cards[i];
+    const currentDataObj = data[i];
+    fillData(currentCard, currentDataObj);
+    removeSkeletons(currentCard);
+  }
+}
+
+populateSkeletons()
