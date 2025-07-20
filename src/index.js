6509b82e958927474eb55e0d900eda4a6572d7bb
devIncogni <arinkumar10@outlook.com>
2025-07-20 21:11:09 +0530

    Added and styled FAQ Section

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ import "./styles/header-styles.css";
 import "./styles/slideshow-styles.css";
 import "./styles/text-underline-animation.css";
 import "./styles/land-card-styles.css";
-import "./styles/why-choose-us.css"
+import "./styles/why-choose-us.css";
 import "./styles/util-styles.css";
 // JS Imports
 import "./scripts/header";
 import "./scripts/loader";
 import "./scripts/slideshow";
-import "./scripts/cards"
+import "./scripts/cards";
+
+const faqs = document.querySelectorAll(".faq-card");
+
+faqs.forEach((faq) => {
+  const aElement = faq.querySelector("a");
+  const faqAns = faq.querySelector(".faq-answer");
+  aElement.addEventListener("click", () => {
+    faqAns.classList.toggle("show");
+  });
+});
