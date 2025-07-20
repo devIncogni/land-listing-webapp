c750c1b1d3b70ee238b13370dc570b5586fb81a5
devIncogni <arinkumar10@outlook.com>
2025-07-20 21:38:48 +0530

    Added FAQ expansion image change

--- src/scripts/faqs.js
+++ src/scripts/faqs.js
@@ -1,4 +1,6 @@
 import faqData from "../faqs.json";
+import plusSVG from "../assets/faq/plus.svg";
+import minusSVG from "../assets/faq/minus.svg";
 
 function populateWithFaqs(numberOfFaqs = 100) {
   const limit = Math.min(numberOfFaqs, faqData.length);
@@ -26,8 +28,14 @@ const faqs = document.querySelectorAll(".faq-card");
 
 faqs.forEach((faq) => {
   const aElement = faq.querySelector("a");
+  const imgElement = faq.querySelector(".faq-question>img");
   const faqAns = faq.querySelector(".faq-answer");
   aElement.addEventListener("click", () => {
     faqAns.classList.toggle("show");
+    if (faqAns.classList.contains("show")) {
+      imgElement.src = minusSVG;
+    } else {
+      imgElement.src = plusSVG;
+    }
   });
 });
