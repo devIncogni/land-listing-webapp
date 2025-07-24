6fda14dafc905a410a2fe4155665dcfd859ed13f
devIncogni <arinkumar10@outlook.com>
2025-07-24 22:09:35 +0530

    Wrote a JS function to render team from JSON Database

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@
 import "./styles/default-reset.css";
 import "./styles/styles.css";
 import "./styles/header-styles.css";
-import "./styles/about-styles.css"
+import "./styles/about-styles.css";
 // import "./styles/slideshow-styles.css";
 import "./styles/text-underline-animation.css";
 // import "./styles/land-card-styles.css";
@@ -16,3 +16,36 @@ import "./scripts/loader";
 // import "./scripts/slideshow";
 // import "./scripts/cards";
 // import "./scripts/faqs";
+
+// Script for automatically rendering team cards
+
+import teamData from "./team-cards.json";
+console.log(teamData);
+
+async function renderTeamCards(numberOfCards = 6) {
+  const finalNum = Math.min(numberOfCards, teamData.length);
+  const teamCardContainer = document.querySelector(".team-card-container");
+  const teamCardTemplate = document.querySelector(".team-card-template");
+  teamCardContainer.textContent = "";
+
+  for (let i = 0; i < finalNum; i += 1) {
+    let teamMemberImg = await import(`./assets/about/${teamData[i].image}`);
+    teamMemberImg = teamMemberImg.default;
+
+    const currentCard = teamCardTemplate.cloneNode(true);
+    const img = currentCard.querySelector("img");
+    const nameSpace = currentCard.querySelector("h3");
+    const designationSpace = currentCard.querySelector("h4");
+    const descriptionSpace = currentCard.querySelector("p");
+
+    img.setAttribute("src", teamMemberImg);
+    nameSpace.textContent = teamData[i].name;
+    designationSpace.textContent = teamData[i].designation;
+    descriptionSpace.textContent = teamData[i].description;
+
+    teamCardContainer.append(currentCard);
+  }
+  //   console.log(teamData.length);
+}
+
+renderTeamCards();
