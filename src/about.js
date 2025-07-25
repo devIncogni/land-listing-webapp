fb8952e69a6126f8d084828bf2ddc75b376a2bbd
devIncogni <arinkumar10@outlook.com>
2025-07-25 20:19:45 +0530

    Made contact us page contact cards

--- src/about.js
+++ src/about.js
@@ -3,19 +3,13 @@ import "./styles/default-reset.css";
 import "./styles/styles.css";
 import "./styles/header-styles.css";
 import "./styles/about-styles.css";
-// import "./styles/slideshow-styles.css";
 import "./styles/text-underline-animation.css";
-// import "./styles/land-card-styles.css";
 import "./styles/why-choose-us.css";
-// import "./styles/faq-styles.css";
 import "./styles/footer.css";
 import "./styles/util-styles.css";
 // JS Imports
 import "./scripts/header";
 import "./scripts/loader";
-// import "./scripts/slideshow";
-// import "./scripts/cards";
-// import "./scripts/faqs";
 
 // Script for automatically rendering team cards
 
---
+++ src/contact.js
@@ -0,0 +1,12 @@
+// CSS Imports
+import "./styles/default-reset.css";
+import "./styles/styles.css";
+import "./styles/header-styles.css";
+import "./styles/contact-us-styles.css";
+import "./styles/text-underline-animation.css";
+import "./styles/why-choose-us.css";
+import "./styles/footer.css";
+import "./styles/util-styles.css";
+// JS Imports
+import "./scripts/header";
+import "./scripts/loader";
